d2bd7fad15876acc9dd5abf8b7b39e2c468d55f1
Malgorzata Phillips <phillipsmalgorzata02@gmail.com>
2023-03-30 09:08:18 -0700

    unique alt attribute for images

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -16,7 +16,7 @@ export default function ProfileHeader() {
     return (
         <div>
             <div>
-                <img className="h-32 w-full object-cover lg:h-28" src={profile.imageUrl} alt="Online portfolio" />
+                <img className="h-32 w-full object-cover lg:h-28" src={profile.imageUrl} alt="Background image" />
 
                 <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
                     <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
@@ -24,7 +24,7 @@ export default function ProfileHeader() {
                             <img
                                 className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
                                 src={profile.coverImageUrl}
-                                alt="Online portfolio"
+                                alt="Avatar"
                             />
                         </div>
                         <div className="mt-6 sm:flex sm:min-w-0 sm:flex-1 sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
