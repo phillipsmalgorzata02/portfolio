fd63cfd617cb55b9ed05b29d652ff36263d234b8
Malgorzata Phillips <phillipsmalgorzata02@gmail.com>
2023-03-30 17:03:41 -0700

    eslint error

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -16,7 +16,7 @@ export default function ProfileHeader() {
     return (
         <div>
             <div>
-                <img className="h-32 w-full object-cover lg:h-28" src={profile.imageUrl} alt="Background image" />
+                <img className="h-32 w-full object-cover lg:h-28" src={profile.imageUrl} alt="Background" />
 
                 <div className="mx-auto max-w-5xl px-4 sm:px-6 lg:px-8">
                     <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
