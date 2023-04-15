14ef3ef54c1afe380733652c128731b19418657f
Malgorzata Phillips <phillipsmalgorzata02@gmail.com>
2023-04-15 15:41:28 -0700

    img path

--- src/components/ButtonCloud.js
+++ src/components/ButtonCloud.js
@@ -103,8 +103,8 @@ export default function ButtonCloud({ category }) {
 
 
     //const buttons = ["angular-icon-256x256-8ljgbnzq.png", "aws-app-icon.jpg", "919826.png", "919827.png", "node-js-512.png", "919830.png", "react--v1.png", "825500.png", "vue-icon-512x512-ne4r9aa8.png", "174881.png"]
-    //const path = "https://malgorzataphillips.netlify.app/assets/images/"
-    const path = "http://localhost:3000/assets/images/"
+    const path = "https://malgorzataphillips.netlify.app/assets/images/"
+    //const path = "http://localhost:3000/assets/images/"
     return (<div>
         {buttons.map((button, index) => {
             return <img alt={index} key={index} className="inline-flex items-center rounded-md px-3 py-2 mr-2 m-1 text-sm font-semibold text-white shadow-sm"
